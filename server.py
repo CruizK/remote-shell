bad457b5cafab7c601806f938470abf87ab8ef6f
CruizK <cruiz.kusman@gmail.com>
2021-02-24 15:17:04 -0600

    Now, with more threads

--- server.py
+++ server.py
@@ -1,30 +1,38 @@
 import os
 import socket
 import subprocess
+import threading
 
 HOST = '127.0.0.1'
 PORT = 2005
 
+def on_client_conn(conn):
+    print('connected by', addr)
+    while True:
+        data = conn.recv(1024)
+        if not data:
+            break
+
+        try:
+            output = subprocess.check_output(data, shell=True, stderr=subprocess.STDOUT)
+
+            if output:
+                conn.sendall(output)
+            else:
+                conn.sendall(b'command complete')
+        
+        except subprocess.CalledProcessError as e:
+            conn.sendall(str(e).encode())
+
+        # print("this is what returned: " + str(output))
+
+
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print('connected by', addr)
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-
-            try:
-                output = subprocess.check_output(data, shell=True, stderr=subprocess.STDOUT)
-
-                if output:
-                    conn.sendall(output)
-                else:
-                    conn.sendall(b'command complete')
-            
-            except subprocess.CalledProcessError as e:
-                conn.sendall(str(e).encode())
-
-            # print("this is what returned: " + str(output))
+    while True:
+        conn, addr = s.accept()
+        thread = threading.Thread(target=on_client_conn, args=(conn,))
+        thread.start()
+        
