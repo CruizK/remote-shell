a5dfbde31c9c5fb144b675f0e8b0ae8b14e61eef
CruizK <cruiz.kusman@gmail.com>
2021-02-24 16:30:29 -0600

    Now properly doesn't let you enter something when not connected, this took way too long to figure out

--- client.py
+++ client.py
@@ -1,15 +1,16 @@
 # import os
 import socket
-
+import sys
 #HOST = '157.230.52.239'
 HOST = '127.0.0.1'
 PORT = 4040
 
-print("enter 'exit' to disconnect")
-user_in = input('$>')
-
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((HOST,PORT))
+        
+        s.connect((HOST, PORT))
+        print("enter 'exit' to disconnect")
+        user_in = input('$>')
+        
         while user_in != "exit":
             s.sendall(bytes(user_in, 'utf-8'))
             data = s.recv(2048)
